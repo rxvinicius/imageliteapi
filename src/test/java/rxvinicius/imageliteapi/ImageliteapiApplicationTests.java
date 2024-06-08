92e24fb5376385ae070eea152f834f68855f9e5d
Vinicius Xavier <vinicius-rodrigues2000@hotmail.com>
2024-06-08 11:15:20 -0300

    Initial commit

---
+++ src/test/java/rxvinicius/imageliteapi/ImageliteapiApplicationTests.java
@@ -0,0 +1,13 @@
+package rxvinicius.imageliteapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageliteapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
